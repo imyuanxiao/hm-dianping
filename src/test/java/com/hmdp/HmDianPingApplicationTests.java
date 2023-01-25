534e68b3596877231eb43eee600f42a73bcdc62a
13524342068 <Gitee19951226>
2023-01-25 13:01:17 +0100

    v2.0 实现全局唯一id生成器RedisIDMaker

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,10 +1,15 @@
 package com.hmdp;
 
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.RedisIDMaker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -12,9 +17,28 @@ class HmDianPingApplicationTests {
     @Resource
     private ShopServiceImpl shopService;
 
+    @Resource
+    private RedisIDMaker redisIDMaker;
+
+    private ExecutorService es = Executors.newFixedThreadPool(300);
+
     @Test
-    void testSaveShop(){
-        shopService.saveShop2Redis(2L, 10L);
+    void testIDMaker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+        Runnable task = () -> {
+            for(int i = 1; i < 100; i++){
+                long l = redisIDMaker.nextID("order:");
+                System.out.println("id = " + l);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for(int i = 0; i < 300; i++){
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
     }
 
 }
